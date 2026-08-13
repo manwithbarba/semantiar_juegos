9b4f3ab7d04dda97963ba3415b16d8c44c7ccb9f
julián Sánchez viamonte <jsanchezviamonte@gmail.com>
2026-08-13 17:14:48 -0300

    Add corpus case navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { BayesGameComponent } from './bayes-game/bayes-game.component';
 import { HomeComponent } from './home/home.component';
 import { CalibrationComponent } from './calibration/calibration.component';
 import { LearningComponent } from './learning/learning.component';
+import { CorpusCasesComponent } from './corpus-cases/corpus-cases.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'casos', component: BayesGameComponent },
   { path: 'calibracion', component: CalibrationComponent },
+  { path: 'casos-reales', component: CorpusCasesComponent },
   { path: 'expresiones', component: LearningComponent, data: { page: 'expresiones' } },
   { path: 'granularidad', component: LearningComponent, data: { page: 'granularidad' } },
   { path: 'atributos', component: LearningComponent, data: { page: 'atributos' } },
