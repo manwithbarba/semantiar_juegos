1f6d2ced7423a5e0c5660372132a7a0bb7011a7d
julián Sánchez viamonte <jsanchezviamonte@gmail.com>
2026-08-13 14:25:19 -0300

    Separate presentation from clinical cases

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideAnimationsAsync(),
+    provideRouter(routes, withHashLocation()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
