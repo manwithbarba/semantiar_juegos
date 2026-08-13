db539a1890fb8fe3c30f5880ed87919f84324d65
julián Sánchez viamonte <jsanchezviamonte@gmail.com>
2026-08-13 15:26:55 -0300

    Add dedicated calibration learning page

---
+++ src/app/calibration/calibration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-calibration',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './calibration.component.html',
+  styleUrl: './calibration.component.css',
+})
+export class CalibrationComponent {}
