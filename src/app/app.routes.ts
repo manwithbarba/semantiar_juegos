6049d7c909e78b7497e92ab875db26e8f2ea1ffa
julián Sánchez viamonte <jsanchezviamonte@gmail.com>
2026-08-13 15:26:51 -0300

    Move calibration into dedicated learning page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BayesGameComponent } from './bayes-game/bayes-game.component';
 import { HomeComponent } from './home/home.component';
+import { CalibrationComponent } from './calibration/calibration.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'casos', component: BayesGameComponent },
+  { path: 'calibracion', component: CalibrationComponent },
   { path: '**', redirectTo: '' },
 ];
